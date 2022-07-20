62b7c2c3ac7a32f2d43257613729e6197dfba1e1
AYO <ayokunmi12@gmail.com>
2022-07-20 20:08:21 +0200

    Add files via upload

---
+++ setup.m
@@ -0,0 +1,57 @@
+function setup
+% Setup script
+% Authors: Pooyan Jamshidi (pooyan.jamshidi@gmail.com)
+% The code is released under the FreeBSD License.
+% Copyright (C) 2018 Pooyan Jamshidi
+
+disp('setting path...');
+pathCell = regexp(path, pathsep, 'split');
+
+myDir = fileparts(mfilename('fullpath'));
+paths = genpath(myDir);
+paths = textscan(paths, '%s', 'delimiter', ':');
+pathsToAdd = [];
+
+if ispc
+    del = '\';
+else
+    del = '/';
+end
+
+for i=1:length(paths)
+    thisPath = paths{i};
+    thisPathSplit = textscan(thisPath, '%s', 'delimiter', del);
+    addThisPath = 1;
+    
+    % Do not add any directories or files starting with a . or a ~ or if it
+    % currently on the path
+    for j=1:length(thisPathSplit)
+        thisStr = thisPathSplit{j};
+        if (~isempty(thisStr)) && ((thisStr(1) == '.') || (thisStr(1) == '~')) || any(strcmpi(thisPath, pathCell))
+            addThisPath = 0;
+        end
+        
+    end
+    if addThisPath ==1
+        if ~isempty(pathsToAdd)
+            thisPath = [':' thisPath];
+        end
+        pathsToAdd = [pathsToAdd thisPath];
+    end
+end
+
+if ~isempty(pathsToAdd)
+    addpath(pathsToAdd);
+end
+
+if ispc
+    rmpath('Controllers\RobusT2Scale')
+else
+    rmpath('Controllers/RobusT2Scale')
+end
+rmpath('FQL4KE')
+end
+
+% if you want to not run this when you exit matlab uncomment this, use
+% this: which pathdef.m -all
+%savepath;
